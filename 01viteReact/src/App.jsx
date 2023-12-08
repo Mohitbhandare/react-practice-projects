dfdcd4a9d3f488ff9630cd57b559ca30ccd80221
Mohit Bhandare <mohitbhandare2001@gmail.com>
2023-12-08 19:02:03 +0530

    update in code

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
+  // Note:- {username} => this is "expression or evalution expression" it means that javascript final outcome which is evaluted and here we can not write javascript code.
+  const username = 'Mohit Bhandare';
+
   return (
     <>
-      <h1>Hello Mohit</h1>
+      <h1>Hello world {username}</h1>
     </>
   );
 }
