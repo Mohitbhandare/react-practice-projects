15aedc6771836bf6156e7a84383fe46cbd0eeb4b
Mohit Bhandare <mohitbhandare2001@gmail.com>
2023-12-09 17:39:35 +0530

    new components added

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <>
-      <h1 className="bg-green-500">Tailwind css</h1>
+      <h1 className="bg-green-500 text-black p-4 rounded-xl mb-4">
+        Tailwind css
+      </h1>
+      <Card userName="Mohit" btnText="click me" />
+      <Card userName="react" btnText="click here" />
     </>
   );
 }
